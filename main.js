66d14bcb5663aebb41a736031037309963c1307b
Ori <198652065@qq.com>
2018-05-16 15:00:06 +0800

    no message

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const schedule = require('node-schedule');
+const fs = require('fs');
 const exec = require('child_process').execSync;
 
 // *    *    *    *    *    *
@@ -14,8 +15,8 @@ const exec = require('child_process').execSync;
 
 // let j = schedule.scheduleJob('0 * * * *', function () {
 //     //init folder date
-//     let date = new Date();
-//     let time = date.getFullYear() + '-' + (date.getMonth() >= 9 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)) + '-' + (date.getDate() > 9 ? date.getDate() : "0" + date.getDate())
+let date = new Date();
+let time = date.getFullYear() + '-' + (date.getMonth() >= 9 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)) + '-' + (date.getDate() > 9 ? date.getDate() : "0" + date.getDate())
 //     let path = "./" + time;
 
 //     console.log(time);
@@ -23,13 +24,35 @@ const exec = require('child_process').execSync;
 
 // });
 
-let git_add = "git add main.js";
+//init folder date
+let path = "./test-report";
+if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach(function (file, index) {
+        var curPath = path + "/" + file;
+        if (fs.statSync(curPath).isDirectory()) { // recurse
+            deleteFolder(curPath);
+        } else { // delete file
+            fs.unlinkSync(curPath);
+        }
+    });
+    fs.rmdirSync(path);
+}
+fs.mkdirSync(path);
+fs.writeFile(path + "/test.txt", date, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+});
+//init data
+
+
+let git_add = "git add " + path + "/test.txt";
 exec(git_add, function (err, stdout, stderr) {
     if (err) {
         console.log('error:' + stderr);
     }
 });
-let git_commit = "git commit  -m  'test.'";
+let git_commit = "git commit  -m '" + time + "'";
 exec(git_commit, function (err, stdout, stderr) {
     if (err) {
         console.log('error:' + stderr);
