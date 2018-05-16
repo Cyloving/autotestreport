4fcd58cad197d1c10ce28aad20799e65563b5be8
Ori <198652065@qq.com>
2018-05-16 15:08:14 +0800

    no message

--- main.js
+++ main.js
@@ -12,60 +12,54 @@ const exec = require('child_process').execSync;
 // │    └──────────────────── minute (0 - 59)
 // └───────────────────────── second (0 - 59, OPTIONAL)
 
+schedule.scheduleJob('0 0 1 * * *', function () {
+    let date = new Date();
+    let time = date.getFullYear() + (date.getMonth() >= 9 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)) + (date.getDate() > 9 ? date.getDate() : "0" + date.getDate())
+    //init folder date
+    console.log("start upload file")
+    let path = "./test-report";
+    if (fs.existsSync(path)) {
+        files = fs.readdirSync(path);
+        files.forEach(function (file, index) {
+            var curPath = path + "/" + file;
+            if (fs.statSync(curPath).isDirectory()) { // recurse
+                deleteFolder(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+    fs.mkdirSync(path);
+    fs.writeFileSync(path + "/test.txt", date, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+    console.log("end upload file")
+    //init data
 
-// let j = schedule.scheduleJob('0 * * * *', function () {
-//     //init folder date
-let date = new Date();
-let time = date.getFullYear() + '-' + (date.getMonth() >= 9 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)) + '-' + (date.getDate() > 9 ? date.getDate() : "0" + date.getDate())
-//     let path = "./" + time;
-
-//     console.log(time);
-
-
-// });
-
-//init folder date
-let path = "./test-report";
-if (fs.existsSync(path)) {
-    files = fs.readdirSync(path);
-    files.forEach(function (file, index) {
-        var curPath = path + "/" + file;
-        if (fs.statSync(curPath).isDirectory()) { // recurse
-            deleteFolder(curPath);
-        } else { // delete file
-            fs.unlinkSync(curPath);
+    console.log("start git")
+    let git_add = "git add " + path + "/test.txt";
+    exec(git_add, function (err, stdout, stderr) {
+        if (err) {
+            console.log('error:' + stderr);
         }
     });
-    fs.rmdirSync(path);
-}
-fs.mkdirSync(path);
-fs.writeFile(path + "/test.txt", date, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-});
-//init data
+    let git_commit = "git commit  -m '" + time + "'";
+    exec(git_commit, function (err, stdout, stderr) {
+        if (err) {
+            console.log('error:' + stderr);
+        }
+    });
 
+    let git_push = "git push";
+    exec(git_push, function (err, stdout, stderr) {
+        if (err) {
+            console.log('error:' + stderr);
+        }
+    });
+    console.log("end git")
 
-let git_add = "git add " + path + "/test.txt";
-exec(git_add, function (err, stdout, stderr) {
-    if (err) {
-        console.log('error:' + stderr);
-    }
-});
-let git_commit = "git commit  -m '" + time + "'";
-exec(git_commit, function (err, stdout, stderr) {
-    if (err) {
-        console.log('error:' + stderr);
-    }
 });
 
-let git_push = "git push";
-exec(git_push, function (err, stdout, stderr) {
-    if (err) {
-        console.log('error:' + stderr);
-    }
-});
 
-// git add file2.txt
-// git add file3.txt
-// git commit  -m  "add 3 files."
